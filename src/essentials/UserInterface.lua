31917cb0cb76f2d5ef4cb8df08dd687676e9c859
wicked-wlzard <00xima16@gmail.com>
2023-04-10 19:00:02 -0400

    Update formatting

--- src/essentials/UserInterface.lua
+++ src/essentials/UserInterface.lua
@@ -1,4 +1,4 @@
-local userInterface = {  __domain = 'local' }
+local userInterface = { __domain = "local" }
 userInterface.__index = userInterface
 
 function userInterface.__initialize(silk)
@@ -7,11 +7,10 @@ function userInterface.__initialize(silk)
 end
 
 function userInterface.typeWrite(instance, text, step)
-
-	if instance:GetAttribute('TypeWriting', true) then
-		instance:SetAttribute('TypeWriting', nil)
+	if instance:GetAttribute("TypeWriting", true) then
+		instance:SetAttribute("TypeWriting", nil)
 	end
-	instance:SetAttribute('TypeWriting', true)
+	instance:SetAttribute("TypeWriting", true)
 
 	instance.MaxVisibleGraphemes = 0
 	instance.Text = text
@@ -20,7 +19,7 @@ function userInterface.typeWrite(instance, text, step)
 	local thread
 
 	connection = instance.AttributeChanged:Connect(function(attribute)
-		if attribute == 'TypeWriting' then
+		if attribute == "TypeWriting" then
 			connection:Disconnect()
 			task.defer(coroutine.close, thread)
 		end
@@ -32,10 +31,20 @@ function userInterface.typeWrite(instance, text, step)
 			task.wait(step or 0.01)
 		end
 		instance.MaxVisibleGraphemes = -1
-		instance:SetAttribute('TypeWriting', nil)
+		instance:SetAttribute("TypeWriting", nil)
 	end)
 	coroutine.resume(thread)
-
 end
 
 return userInterface
+
+--[=[
+		UI related methods.
+		
+		| Package Attribute | Value |
+		| --- | --- |
+		| __singleton | false |
+		| __domain | local |
+
+		@class UserInterface
+]=]
